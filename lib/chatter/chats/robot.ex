7602f9ee96447a69bb879346e6472e72b99c3e62
ryankc33 <ryachew.dev@gmail.com>
2020-12-02 14:46:21 +0800

    handle questions

--- lib/chatter/chats/robot.ex
+++ lib/chatter/chats/robot.ex
@@ -8,17 +8,19 @@ defmodule Chatter.Chats.Robot do
     cond do
       is_a_ping(body) -> "Pong!"
       wants_a_diamond(body) -> Diamond.call(body)
-      do_we_know(body) -> "I'll find out for you!"
+      is_a_question(body) -> "I don't know, but I'll find out and let you know!"
       is_a_greeting(body) -> randomized_greeting()
       true -> body
     end
   end
 
   defp is_a_ping(body), do: String.contains?(body, "ping!")
-  defp do_we_know(body), do: String.contains?(body, "do") && String.contains?(body, "know")
+
+  @questions ["who", "what", "where", "when", "why", "how"]
+  defp is_a_question(body), do: String.contains?(body, @questions)
   defp wants_a_diamond(body), do: String.contains?(body, "diamond")
 
   @greetings ["hello", "hey", "hi", "bonjour", "guten tag", "你好"]
-  defp is_a_greeting(body), do: body in @greetings
+  defp is_a_greeting(body), do: String.contains?(body, @greetings)
   defp randomized_greeting(), do: Enum.at(@greetings, :rand.uniform(5)) <> "!"
 end
